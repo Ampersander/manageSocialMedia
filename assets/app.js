a04123d892d0db2ef3ba0de6e8d54334ccb089ba
Yuyari SALAS <yuyari.salas@etu.univ-orleans.fr>
2021-03-22 16:10:41 +0100

    Connection Facebook

--- assets/app.js
+++ assets/app.js
@@ -14,11 +14,11 @@ import './bootstrap';
 // Renvoie le user short-lived token à partid du appId, puis déconnecte la session
 async function sendFacebookToken(appId) {
     console.log('Connexion à Facebook, appId : ' + appId);
-    FB.init({ appId: appId, status: false, xfbml: false, version: 'v10.0' });
+    FB.init({ appId: appId, status: true, xfbml: false, version: 'v10.0' });
     FB.login(function (response) {
         if (response.authResponse) {
             console.log('Connecté à Facebook ! Access token : ' + response.authResponse.accessToken);
-            $('access-token').text(response.authResponse.accessToken);
+            $('#access-token').text(response.authResponse.accessToken);
             // Envoi du short-lived token au serveur
             // let data = {
             //     appId: appId,
@@ -35,20 +35,9 @@ async function sendFacebookToken(appId) {
             console.log('Connection échouée');
         }
     });
-}
-
-
+};
 
 $(function () {
-
-    // Déconnecte l'utilisateur pour 
-    FB.getLoginStatus(function (response) {
-        if (response.status === 'connected') {
-            $('access-token').text(response.authResponse.accessToken);
-        } else if (response.status === 'not_authorized') {
-            $('access-token').text(response.authResponse.accessToken);
-        }
-    });
 
     $('#fb-log-btn').on('click', async function () {
         let appId = $('#fb-app-id').val();
