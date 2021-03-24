7d7d6ba3a6de85827f05b0dc98af08bc77bec55c
Yuyari SALAS <yuyari.salas@etu.univ-orleans.fr>
2021-03-24 17:34:34 +0100

    Ajouts des dernières vérifs sur les images, ajout host API ImgBB

--- src/Utility/TwitterAPI.php
+++ src/Utility/TwitterAPI.php
@@ -16,6 +16,51 @@ class TwitterAPI
     }
 
     /**
+     * Upload des photos sur un hébergeur et stockage local en vue d'une publication sur Twitter
+     * @param array $images Liste d'images à stocker et heberger (envoyer $form->getData())
+     * @return array $names Retourne la liste du nom des images stockées localement
+     */
+    public function stockImages($images)
+    {
+        $names = [];
+        try {
+            // Vérif nombres d'images
+            if (sizeof($images) > 4) throw new \Exception('Echec de l\'envoi du post sur Twitter, 4 images maximum autorisées');
+            
+            foreach ($images as $image) {
+                $ext = $image->guessExtension();
+                // Stockage en local
+                $folder = $this->parameterBag->get('kernel.project_dir') . '/public/twitter/';
+                $imgName = uniqid() . $ext;
+                $imgPath = $folder . $imgName;
+                $image->move($folder, $imgPath);
+                // Vérif ext image
+                if ($ext != 'jpg' && $ext != 'jpeg' && $ext != 'png' && $ext != 'gif' && $ext != 'webp') {
+                    unlink($imgPath);
+                    foreach ($names as $name) {
+                        unlink($folder . $name);
+                    }
+                    throw new \Exception('Echec de l\'envoi du post sur Twitter : format d\'image ' . $ext . 'non supporté, formats acceptés = JPG, PNG, GIF, WEBP');
+                };
+                // Vérif poids image
+                $fileSize = filesize($imgPath);
+                if ($fileSize > 5 * (10 ** 6)) {
+                    unlink($imgPath);
+                    foreach ($names as $name) {
+                        unlink($folder . $name);
+                    }
+                    throw new \Exception('Echec de la publication de la photo sur Twitter : image trop volumineuse, limite de taille = 5MB, taille de l\'image = ' . $fileSize . 'B');
+                };
+                // Stockage du path dans la valeur retour                
+                $names[] = $imgName;
+            }
+        } catch (\Exception $e) {
+            throw $e;
+        }
+        return $names;
+    }
+
+    /**
      * Publie un statut avec ou sans photos sur la page Twitter
      * @param array $imagePaths Liste des chemins (absolus) des images, ne rien mettre si pas d'images
      */
@@ -27,17 +72,6 @@ class TwitterAPI
             if ($message && strlen($message) > 280) {
                 throw new \Exception('Echec de la publication de la photo sur Twitter : message trop long, limite de caractères = 280');
             };
-            // Trop de photos
-            if ($imagePaths && sizeof($imagePaths) > 4) {
-                throw new \Exception('Echec de l\'envoi du post sur Twitter : trop d\'images, maximum = 4');
-            }
-            // Image trop volumineuse
-            foreach ($imagePaths as $imgPath) {
-                $fileSize = filesize($imgPath);
-                if ($fileSize > 10 * (10 ** 6)) {
-                    throw new \Exception('Echec de la publication de l\'image sur Facebook : image trop volumineuse, limite de taille = 10MB, taille de l\'image = ' . $fileSize . 'B');
-                };
-            }
 
             // Request
             $connection = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);
